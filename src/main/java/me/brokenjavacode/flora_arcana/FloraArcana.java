e14773596673a65841fbce324f858cd875b4aa83
Broken-java-code <d0m1nus048@gmail.com>
2025-02-22 16:22:42 +0100

    Uploaded

---
+++ src/main/java/me/brokenjavacode/flora_arcana/FloraArcana.java
@@ -0,0 +1,16 @@
+package me.brokenjavacode.flora_arcana;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FloraArcana implements ModInitializer {
+	public static final String MOD_ID = "flora-arcana";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/me/brokenjavacode/flora_arcana/FloraArcanaClient.java
@@ -0,0 +1,10 @@
+package me.brokenjavacode.flora_arcana;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FloraArcanaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
